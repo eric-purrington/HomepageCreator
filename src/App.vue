dea86cb81e43ce0f9fd3fdc84e8d9cdc5d82939d
Seth Purr <eric.purrington@gmail.com>
2020-11-19 19:41:02 -0700

    some basic questions asked on input page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="inputPage" v-if="!inputRecieved">
+        <label for="name">Name to be displayed</label>
+        <input type="text" class="name" placeholder="" v-model="info.name"/>
+        <label for="zipcode">Zipcode for weather</label>
+        <input type="text" class="zipcode" placeholder="98125" v-model="zipcode"/>
+        <label for="links">Links on your homepage</label>
+        <input type="text" class="links" placeholder="youtube.com"/>
+
+
+    </div>
+    <DisplayPage info="info" :render="inputRecieved"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import DisplayPage from './components/DisplayPage.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            DisplayPage
+        },
+        data () {
+          return {
+            inputRecieved: false,
+            zipcode: '',
+            info: {
+              name: '',
+              date: '',
+              time: '',
+              weather: {
+
+              },
+              links: [
+
+              ],
+              background: ''
+            }
+          }
+        },
+        methods: {
+            // fetchWeather (e) {
+            //   fetch("https")
+            // },
+            // fetchFavicons () {
+
+            // },
+            // savePreferences () {
+                //save to browser cache and change inputRecieved
+            // }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+    label, input {
+      display: flex;
+      margin: 30px;
+    }
 </style>
